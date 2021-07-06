27b77116cdcb726f7909d0247153d41e56fb95dd
Christopher <cwpredovich@yahoo.com>
2021-07-06 17:38:56 -0600

    sytling changes

--- skyfineusa-interview-project/src/App.js
+++ skyfineusa-interview-project/src/App.js
@@ -1,18 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './police-car-stock-photo.jpeg'
 import Table from './Table';
 
 function App() {
   return (
 
     <div className="App">
+      <div>
+        {/* <img src={logo} alt="police car" height="100%" width="100%" /> */}
+      </div>
       <header className="App-header">
         <h1>State Population and DUI Data</h1>
         <h3>Answering the question, where is SkyFineUSA needed most urgently?</h3>
       </header>
-      <Table />
-      <p>Population data is from <a href="https://www.infoplease.com/us/states/state-population-by-rank">https://www.infoplease.com/us/states/state-population-by-rank</a></p>
-      <p>DUI data is from <a href="https://backgroundchecks.org/which-states-have-the-worst-dui-problems.html">https://backgroundchecks.org/which-states-have-the-worst-dui-problems.html</a></p>
+      <div className="main">
+        <Table />
+        <p>
+        Population data is from <a href="https://www.infoplease.com/us/states/state-population-by-rank">https://www.infoplease.com/us/states/state-population-by-rank</a>
+        </p>
+        <p>
+        DUI data is from <a href="https://backgroundchecks.org/which-states-have-the-worst-dui-problems.html">https://backgroundchecks.org/which-states-have-the-worst-dui-problems.html</a>
+        </p>  
+      </div>    
     </div>
   );
 }
--- skyfineusa-interview-project/src/Table.js
+++ skyfineusa-interview-project/src/Table.js
@@ -1,4 +1,3 @@
-// import React, { useState } from 'react';
 import { useState } from 'react';
 import { TableWithBrowserPagination, Column } from 'react-rainbow-components';
 import StatesData from './StatesData';
@@ -7,16 +6,19 @@ const data = StatesData;
 
 const Table = () => {
 
-    const [key, setKey] = useState("duiSeverityScore")
+    const [tableData, setTableData] = useState(data)
+
+
 
     return (
         <div>
-            
-            <p>Hey, it's a paginated table!</p>
+            <div className="table-description">
+                <p>The table below displays all 50 United States in order of their DUI Severity Score. The DUI Severity Score is calculated using DUI arrest rates per 100K and DUI fatalities per 100K. Rates per 100K were calculated using the latest 2018 FBI Arrest statistics for DUI arrests, and National Highway Traffic Safety Administration statistics for DUI fatalities.</p>
+            </div>
 
             <TableWithBrowserPagination 
                 className = ""
-                data = {data}
+                data = {tableData}
                 pageSize = {10}
                 keyField = "state"
             >
